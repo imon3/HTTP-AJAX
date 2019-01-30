51d8b9e7c47be265619037a4dcda23339250a359
Imon Ovbude <imonovbude@gmail.com>
2019-01-30 13:14:43 -0800

    pulled data with axios and set to state, mapped over friends array

---
+++ friends/src/components/Friends.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import axios from 'axios';
+
+class Friends extends React.Component {
+    state = {
+        friends: []
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:5000/friends')
+            .then(res => {
+                this.setState({
+                    friends: res.data
+                })
+            })
+            .catch(err => err)
+    }
+
+    render() {
+        console.log(this.state.friends)
+        return (
+            <div>
+                {this.state.friends.map(friend => {
+                    return (
+                        <div key={friend.id}>
+                            <p>name: {friend.name}</p>
+                            <p>age: {friend.age}</p>
+                            <p>email: {friend.email}</p>
+                        </div>
+                    )
+                })}
+            </div>
+        )
+    }
+}
+
+export default Friends;
