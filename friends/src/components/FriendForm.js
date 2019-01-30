b3469caf173b2ecce1b03c0ca683df6f24bb6034
Imon Ovbude <imonovbude@gmail.com>
2019-01-30 15:41:43 -0800

    styled friend form

--- friends/src/components/FriendForm.js
+++ friends/src/components/FriendForm.js
@@ -1,27 +1,31 @@
 import React from 'react';
+import './FriendForm.css'
 
 class FriendForm extends React.Component {
 
     render() {
         return (
-            <form className='friend-form'>
-                <div>
-                    <label htmlFor='name'>Enter Name: </label>
-                    <input id='name' type='text' name='name' />
-                </div>
+            <div className='form-wrapper'>
+                <form className='friend-form'>
+                    <div>
+                        <label htmlFor='name'>Enter Name: </label>
+                        <input id='name' type='text' name='name' />
+                    </div>
 
-                <div>
-                    <label htmlFor='age'>Enter Age: </label>
-                    <input id='age' type='number' name='age' />
-                </div>
+                    <div>
+                        <label htmlFor='age'>Enter Age: </label>
+                        <input id='age' type='number' name='age' />
+                    </div>
 
-                <div>
-                    <label htmlFor='email'>Enter Email: </label>
-                    <input id='email' type='text' name='email' />
-                </div>
+                    <div>
+                        <label htmlFor='email'>Enter Email: </label>
+                        <input id='email' type='text' name='email' />
+                    </div>
 
-                <button>Submit</button>
-            </form>
+                    <button>Submit</button>
+                </form>
+            </div>
+
         )
     }
 }
